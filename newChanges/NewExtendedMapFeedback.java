2ccd549e64e665a6b827c72861e0251791473874
Horstexplorer <21320859+Horstexplorer@users.noreply.github.com>
2019-11-27 18:11:17 +0100

    removed unused

--- newChanges/NewExtendedMapFeedback.java
+++
@@ -1,9 +0,0 @@
-package newChanges;
-
-import freemind.modes.ExtendedMapFeedback;
-import freemind.modes.MindMap;
-
-public interface NewExtendedMapFeedback extends ExtendedMapFeedback {
-
-    NewMindMapNode newNode(Object pUserObject, MindMap pMap);
-}
