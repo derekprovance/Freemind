a6a4290a112a8603ecdf6818c6d611dbd4156975
Horstexplorer <21320859+Horstexplorer@users.noreply.github.com>
2019-12-04 12:56:18 +0100

    Change can be partially undone

--- newChanges/NodeWrapper.java
+++ newChanges/NodeWrapper.java
@@ -33,9 +33,13 @@ public class NodeWrapper implements MindMapNodeExt{
             this.nodeAdapter.setColor(Color.MAGENTA);
             this.nodeAdapter.setUnderlined(true);
             this.nodeAdapter.setBold(true);
-            this.nodeAdapter.setNoteText(">> RESOURCE");
+            this.nodeAdapter.setNoteText(">> RESOURCE \n"+this.nodeAdapter.getText());
             ANSManager.setLastResourceNode(this);
         }else{
+            this.nodeAdapter.setColor(Color.BLACK);
+            this.nodeAdapter.setUnderlined(false);
+            this.nodeAdapter.setBold(false);
+            this.nodeAdapter.setNoteText(this.nodeAdapter.getText().replace(">> RESOURCE\n",""));
             ANSManager.setLastResourceNode(null);
         }
     }
